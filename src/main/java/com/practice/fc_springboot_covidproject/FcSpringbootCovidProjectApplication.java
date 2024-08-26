31d2df731224e27a3d843b7bca76c19718fd19f7
Delavuida <jaju1995@naver.com>
2024-08-26 12:04:30 +0900

    springBoot setup
    springboot setup

---
+++ src/main/java/com/practice/fc_springboot_covidproject/FcSpringbootCovidProjectApplication.java
@@ -0,0 +1,13 @@
+package com.practice.fc_springboot_covidproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcSpringbootCovidProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcSpringbootCovidProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/fc_springboot_covidproject/FcSpringbootCovidProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.fc_springboot_covidproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcSpringbootCovidProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
